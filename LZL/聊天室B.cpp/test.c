3a9d5e8179e09338bae03188332817dd1e8aad23
zhaolonga <2339824768@qq.com>
2019-08-17 00:09:01 +0800

    界面修改

--- LZL/聊天室B.cpp/test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-char buf[200]="okokokoko";
-int main()
-{
-    printf("\033[31m %s \033[0m \n",buf);
-	printf("\033[32m *%s* \033[0m \n",buf);
-	printf("\033[35m *你要改变颜色的内容，黄底和紫色字* \033[0m \n");
-}
